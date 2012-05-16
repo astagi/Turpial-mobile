df4c73c1cf65e44a543d4917cfe66e2951f87cb7
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-05-17 00:01:02 +0200

    Script ridtabs now acts on css files too

--- scripts/ridtabs.sh
+++ scripts/ridtabs.sh
@@ -3,8 +3,9 @@
 # Author: Andrea Stagi (4ndreaSt4gi)
 # Description: converts tabs in 4 spaces in every source file. Use it before commit!
 # License: GPL v3
+
 cd ..
-for fname in `find . -type f -regex ".*\.\(js\|java\|html\|xml\)"`
+for fname in `find . -type f -regex ".*\.\(js\|java\|html\|xml\|css\)"`
 do
     expand -t 4 $fname > aux.txt
     cat aux.txt > $fname
